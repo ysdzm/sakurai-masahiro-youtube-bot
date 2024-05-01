a76f0865ca051b3f8c502bcfc87e4d019be46e94
dokudami <69578376+ysdzm@users.noreply.github.com>
2024-05-01 14:44:16 +0900

    discordに通知する処理を追加

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from googleapiclient.discovery import build
 
 def get_api_key():
@@ -6,6 +7,12 @@ def get_api_key():
     if not api_key:
         raise ValueError('YouTube API key not found in environment variables.')
     return api_key
+
+def get_webhook_url():
+    webhook_url = os.environ.get('WEBHOOK_URL')
+    if not webhook_url:
+        raise ValueError('Discord webhook URL not found in environment variables.')
+    return webhook_url
 
 def get_new_videos(api_key, channel_id, max_results=10):
     # YouTube Data APIのビルド
@@ -46,7 +53,7 @@ def get_latest_video_id(filename='latest_video_id.txt'):
 # APIキーを取得
 api_key = get_api_key()
 
-# チャンネルIDを指定
+# 桜井政博のゲーム作るにはのチャンネルIDを指定
 channel_id = 'UC5CTV3JSdrlo5Pa42QkK8SA'
 
 # 最新の動画のVideo IDを取得
@@ -58,7 +65,14 @@ new_videos = get_new_videos(api_key, channel_id)
 # 最新の動画よりも古い動画のVideo IDを出力し、Video IDを更新
 for video in new_videos:
     if video['video_id'] != latest_video_id:
-        print('Video ID:', video['video_id'])
+        # Discordにメッセージを送信
+        webhook_url = get_webhook_url()
+        message = {
+            "username": "桜井政博のゲーム作るにはbot", 
+            "avatar_url": "https://yt3.googleusercontent.com/aoouNzNe8R3Blcmy8EghybnFbCsbexbjK6gBi6Mapli0xpt5r-mubIpODo1IGPr54eyULZs=s176-c-k-c0x00ffffff-no-rj", 
+            "content": f"New video: {video['title']} - https://www.youtube.com/watch?v={video['video_id']}"
+            }
+        requests.post(webhook_url, json=message)
     else:
         break
 
